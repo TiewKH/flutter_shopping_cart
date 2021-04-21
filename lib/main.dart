97a90cbcaf8c1f7c07b16e799a6c62e7b7c894be
TiewKH <tiewkeehui95@hotmail.com>
2021-04-21 20:40:38 +0800

    Upgrade for Flutter 2.0.5

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_shopping_cart/redux/store.dart';
 import 'package:flutter_shopping_cart/main/shopping_cart_app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final store = await createReduxStore();
   runApp(ShoppingCartApp(store));
 }
